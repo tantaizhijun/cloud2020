16497908e532f761f23cb07d3e023970fb4c3331
tanzj <tanzj@asiainfo.com>
2020-06-29 10:37:40 +0800

    8001服务注册进eureka

--- cloud-provider-payment8001/src/main/java/com/test/springcloud/PaymentApplication8001.java
+++ cloud-provider-payment8001/src/main/java/com/test/springcloud/PaymentApplication8001.java
@@ -2,9 +2,10 @@ package com.test.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 //@EnableDiscoveryClient
 public class PaymentApplication8001 {
     public static void main(String[] args) {
