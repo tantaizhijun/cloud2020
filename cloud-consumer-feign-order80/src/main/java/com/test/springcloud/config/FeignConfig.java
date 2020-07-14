c8b18a50577d586c458cabcf5d216bd690bfda15
tanzj <tanzj@asiainfo.com>
2020-07-14 17:33:10 +0800

    openFeign的日志级别设置

---
+++ cloud-consumer-feign-order80/src/main/java/com/test/springcloud/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.test.springcloud.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+
+    @Bean
+    Logger.Level feignLoggerlevel(){
+        return Logger.Level.FULL;
+    }
+}
