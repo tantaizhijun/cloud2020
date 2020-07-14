b947cd973d3600d3a751f9e868e43fe1298ca640
tanzj <tanzj@asiainfo.com>
2020-07-14 17:01:46 +0800

    通过openFeign调用服务接口 自带负载均衡功能

---
+++ cloud-consumer-feign-order80/src/main/java/com/test/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,15 @@
+package com.test.springcloud.service;
+
+import com.test.springcloud.entities.ResultData;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    ResultData get(@PathVariable("id") Long id);
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/test/springcloud/web/OrderFeignWeb.java
@@ -0,0 +1,23 @@
+package com.test.springcloud.web;
+
+import com.test.springcloud.entities.ResultData;
+import com.test.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignWeb {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public ResultData getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.get(id);
+    }
+}
